080e4c79ecad4a92fd265761bd94d6a03cc490df
Jason Lemay <jlemay@godaddy.com>
2020-11-24 14:05:17 -0500

    🎉 Init Stylelint Config

---
+++ README.md
@@ -0,0 +1,23 @@
+# stylelint-config
+
+A repository for CSS and Sass linting rules that have been derived from best practices.
+
+## The rules `index.js` file
+
+The rules for our style linting are defined in `index.js`.
+
+## Installation
+
+```
+npm i --save-dev stylelint @godaddy-wordpress/stylelint-config
+```
+
+## Usage
+
+Create a `.stylelintrc` file in the root of your project and extend the config file:
+
+``` json
+{
+  "extends": "@godaddy-wordpress/stylelint-config"
+}
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+"use strict"
+
+module.exports = {
+  "extends": "stylelint-config-wordpress/scss",
+  "plugins": [ "stylelint-order" ],
+  "rules": {
+    "at-rule-empty-line-before": [
+      "always",
+      {
+        "except": [ "after-same-name" ],
+        "ignore": [ "after-comment", "first-nested" ],
+        "ignoreAtRules": [ "else" ]
+      }
+    ],
+
+    "function-calc-no-invalid": null,
+    "function-url-quotes": "never",
+
+    "max-line-length": null,
+
+    "no-descending-specificity": null,
+
+    "number-leading-zero": "always",
+
+    "order/properties-alphabetical-order": true,
+
+    "rule-empty-line-before": [
+      "always",
+      {
+        "ignore": [ "after-comment", "first-nested", "inside-block" ]
+      }
+    ],
+
+    "scss/selector-no-redundant-nesting-selector": null,
+
+    "selector-class-pattern": null,
+    "selector-id-pattern": null,
+    "selector-type-no-unknown": null
+  }
+};
