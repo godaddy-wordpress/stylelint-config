c388e65736805f1cdf9b78523d778241e051bde5
Olivier Lafleur <olafleur@godaddy.com>
2022-02-14 16:36:14 -0500

    Fix postcss migration in CoBlocks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 
 module.exports = {
-  "extends": "@wordpress/stylelint-config/scss",
+  "extends": ["@wordpress/stylelint-config/scss", "stylelint-config-standard-scss"],
   "plugins": [ "stylelint-order" ],
   "rules": {
     "at-rule-empty-line-before": [
@@ -34,6 +34,35 @@ module.exports = {
 
     "selector-class-pattern": null,
     "selector-id-pattern": null,
-    "selector-type-no-unknown": null
+    "selector-type-no-unknown": null,
+    "scss/double-slash-comment-whitespace-inside": null,
+    "indentation": null,
+    "scss/no-global-function-names": null,
+    "scss/at-mixin-argumentless-call-parentheses": null,
+    "alpha-value-notation": null,
+    "color-function-notation": null,
+    "hue-degree-notation": null,
+    "number-max-precision": null,
+    "block-closing-brace-empty-line-before": null,
+    "scss/double-slash-comment-empty-line-before": null,
+    "value-keyword-case": null,
+    "shorthand-property-no-redundant-values": null,
+    "max-empty-lines": null,
+    "selector-descendant-combinator-no-non-space": null,
+    "selector-no-vendor-prefix": null,
+    "no-invalid-position-at-import-rule": null,
+    "property-no-vendor-prefix": null,
+    "keyframes-name-pattern": null,
+    "declaration-empty-line-before": null,
+    "scss/dollar-variable-colon-space-after": null,
+    "declaration-block-no-redundant-longhand-properties": null,
+    "scss/at-mixin-pattern": null,
+    "scss/dollar-variable-pattern": null,
+    "scss/at-import-partial-extension": null,
+    "scss/no-duplicate-mixins": null,
+    "media-feature-parentheses-space-inside": null,
+    "value-no-vendor-prefix": null,
+    "custom-property-pattern": null,
+    "scss/dollar-variable-empty-line-before": null
   }
 };
